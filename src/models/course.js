26f5b1196e8cfb4447a96e122f88213323b19828
abcde090 <229243386@qq.com>
2020-01-31 19:10:28 +1000

    Initialize server Create course, person, student, teacher, user models.

---
+++ src/models/course.js
@@ -0,0 +1,51 @@
+/* eslint-disable func-names */
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const courseSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      uppercase: true,
+      alias: 'code'
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      validate: {
+        validator: url => !Joi.validate(url, Joi.string().uri()).error,
+        msg: 'Invalid url format'
+      }
+    },
+    __v: { type: Number, select: false }
+  },
+  {
+    timestamps: true,
+    toObject: {
+      virtuals: true
+    },
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+courseSchema.statics.searchQuery = async function(pagination, sort, search) {
+  const { page, pageSize } = pagination;
+  return this.find({ _id: { $regex: search, $options: 'i' } })
+    .sort(sort)
+    .skip((page - 1) * pageSize)
+    .limit(pageSize);
+};
+
+module.exports = mongoose.model('Course', courseSchema);
