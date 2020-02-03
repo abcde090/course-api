7f10016cd8d7ecb62456fcdd444db2224b5af208
abcde090 <229243386@qq.com>
2020-02-03 21:48:59 +1000

    Update the server

--- src/server.js
+++ src/server.js
@@ -1,53 +1,57 @@
-const express = require('express');
-require('envdotjson').load();
+require('dotenv').config();
 require('express-async-errors');
+const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-
+const logger = require('./utils/logger');
 const swaggerSpec = YAML.load('./swagger/swagger.yaml');
 const routes = require('./routes');
+
 const { connectToDB } = require('./utils/db');
 const errorHandler = require('./middleware/errorHandler');
-const logger = require('./utils/logger');
+
 
 process.on('uncaughtException', e => {
   logger.error(e.message);
   setTimeout(() => process.exit(1), 1000);
 });
 
+
 process.on('unhandledRejection', e => {
   logger.error(e.message);
   setTimeout(() => process.exit(1), 1000);
 });
 
+
 const app = express();
 const PORT = process.env.PORT || 3111;
-const morganLvl = process.env.NODE_ENV === 'production' ? 'short' : 'dev';
-const morganLog = morgan(morganLvl, { stream: logger.stream });
+const morganLog =
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(helmet());
-app.use(cors());
 app.use(morganLog);
+app.use(cors());
 app.use(express.json());
-app.use('/v1', routes);
+
+app.use('/api', routes);
 app.use(errorHandler);
 
 connectToDB()
   .then(() => {
-    logger.info('DB connected');
-    if (process.env.NODE_ENV !== 'test') {
-      app.listen(PORT, () => {
-        logger.info(`Server is listening on PORT: ${PORT}`);
-      });
-    }
+    console.log('DB connected');
+    app.listen(PORT, () => {
+      logger.info(`Server is listening on PORT: ${PORT}`);
+    });
   })
   .catch(e => {
     logger.error('DB connection failed');
     throw new Error(e);
   });
+
 
 module.exports = app;
